2b581111e008acf04bb51a3df183d813914254f9
Linganagouda <linganagoudaam@gmail.com>
2026-07-17 22:22:47 +0530

    Initial Spring Boot setup

---
+++ src/main/java/com/smartbank/smart_bank_backend/SmartBankBackendApplication.java
@@ -0,0 +1,13 @@
+package com.smartbank.smart_bank_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartBankBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartBankBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/smartbank/smart_bank_backend/SmartBankBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smartbank.smart_bank_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartBankBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
