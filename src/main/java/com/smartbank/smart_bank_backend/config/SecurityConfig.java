9665df9f271ebc5822b93184bd739a7a60c6822e
Linganagouda <linganagoudaam@gmail.com>
2026-08-07 19:27:16 +0530

    Implemented CustomUserDetailsService

---
+++ src/main/java/com/smartbank/smart_bank_backend/config/SecurityConfig.java
@@ -0,0 +1,58 @@
+package com.smartbank.smart_bank_backend.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    private final PasswordEncoder passwordEncoder;
+
+    public SecurityConfig(PasswordEncoder passwordEncoder) {
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http)
+            throws Exception {
+
+        http
+
+                .csrf(csrf -> csrf.disable())
+
+                .sessionManagement(session ->
+                        session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+
+                .authorizeHttpRequests(auth -> auth
+
+                        .requestMatchers(
+                                "/api/auth/**"
+                        ).permitAll()
+
+                        .anyRequest().authenticated()
+
+                )
+
+                .httpBasic(Customizer.withDefaults());
+
+        return http.build();
+
+    }
+
+    @Bean
+    AuthenticationManager authenticationManager(
+            AuthenticationConfiguration configuration)
+            throws Exception {
+
+        return configuration.getAuthenticationManager();
+
+    }
+
+}
--- src/main/java/com/smartbank/smart_bank_backend/security/CustomUserDetailsService.java
+++ src/main/java/com/smartbank/smart_bank_backend/security/CustomUserDetailsService.java
@@ -1,5 +1,40 @@
 package com.smartbank.smart_bank_backend.security;
 
-public class CustomUserDetailsService {
+import java.util.Collections;
+
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.smartbank.smart_bank_backend.repository.UserRepository;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    public CustomUserDetailsService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String email)
+            throws UsernameNotFoundException {
+
+        com.smartbank.smart_bank_backend.entity.User user =
+                userRepository.findByEmail(email)
+                        .orElseThrow(() ->
+                                new UsernameNotFoundException("User not found"));
+
+        return User.builder()
+                .username(user.getEmail())
+                .password(user.getPassword())
+                .authorities(
+                        Collections.singletonList(() ->
+                                "ROLE_" + user.getRole().getRoleName()))
+                .build();
+    }
 
 }
